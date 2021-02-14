d7345df38f8e41431552387d377b889ea519eb85
Max Irendorfer <e1634015@student.tuwien.ac.at>
2021-02-14 21:30:25 +0100

    day 1

---
+++ solutions/day_01/exercises1/level1.js
@@ -0,0 +1,58 @@
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya',
+];
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB',
+];
+
+//1.
+const empty = [];
+//2.
+const numbers = [1, 2, 3, 4, 5];
+//3.
+console.log(numbers.length);
+//4.
+console.log(numbers[0], numbers[length]);
+//5.
+const mixedDataTypes = [
+  'word 1',
+  2,
+  3.5,
+  'word2',
+  { food: 'pizza', ingredients: 'chesse' },
+];
+console.log(mixedDataTypes.length);
+
+//6.
+const itCompanies = [
+  'Facebook',
+  'Google',
+  'Microsoft',
+  'Apple',
+  'IBM',
+  'Oracle',
+  'Amazon',
+];
+
+// 7.
+console.log(itCompanies);
+
+// 8.
+console.log(itCompanies.length);
